3e0af352ef222dbf53fc0f6a60bc7fd02f5b51ab
Thulani <ltmaseko7@gmail.com>
2024-04-06 19:20:00 +0200

    Scaping Paginate content

--- scrapePages.py
+++ scrapePages.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+baseUrl = 'https://scrapingclub.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 items = soup.find_all('div', class_='w-full rounded border')
@@ -14,6 +15,19 @@ for i in items:
 links = []
 urls = soup.find_all('span', class_='page')
 for url in urls:
-    if(url.find('a') != None):
-        print(url.find('a').text)
-        x = url.find('a')
+    aTag = url.find('a') 
+    if(aTag != None):
+        if aTag.text.isdigit():  # (('Next' not in url.find('a')) and ('Prev' not in url.find('a'))):
+            x = aTag.get('href')
+            links.append(x)
+
+for link in links:
+    newUrl = baseUrl + link
+    response = requests.get(newUrl)
+    soup = BeautifulSoup(response.text, 'lxml')
+    items = soup.find_all('div', class_='w-full rounded border')
+    for i in items:
+        itemName = i.find('h4').text.strip('\n')
+        itemPrice = i.find('h5').text
+        print('%s ) Price: %s, Item Name %s' % (count, itemPrice, itemName))
+        count += 1
