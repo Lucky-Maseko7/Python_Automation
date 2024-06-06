c6d548efbeb7f964266b882d2a7b3ed1e2f4cf3a
Thulani <ltmaseko7@gmail.com>
2024-06-06 22:08:31 +0200

    Code Refactoring

--- API/apiCall.py
+++ API/apiCall.py
@@ -1,17 +1,33 @@
 import requests
-import json
-baseUrl = 'https://api.upcitemdb.com/prod/trial/lookup'
-# parameters = {'upc': '0012993441012'}
-parameters = {'upc': '073366118238'}
-response = requests.get(baseUrl, params=parameters)
-print(response.url)
-content = response.content
-info = json.loads(content)
-print(type(info))
-print(info)
-items = info['items']
-itemInfo = items[0]
-title = itemInfo['title']
-brand = itemInfo['brand']
-print(title)
-print(brand)
+
+
+# Define a function that makes an API call to the UPC item database
+# and returns the title and brand of the item with the given UPC code.
+def api_call(upc):
+    # Set the base URL for the API request
+    base_url = 'https://api.upcitemdb.com/prod/trial/lookup'
+    # Send a GET request to the API with the UPC code as a parameter
+    response = requests.get(base_url, params={'upc': upc})
+    # Raise an exception if the request was unsuccessful
+    response.raise_for_status()
+    # Parse the JSON response into a Python dictionary
+    data = response.json()
+    # Get the list of items from the response
+    items = data['items']
+    # If there is at least one item, return its title and brand
+    if items:
+        item_info = items[0]
+        return item_info['title'], item_info['brand']
+    # If there are no items, return None for both title and brand
+    return None, None
+
+
+if __name__ == '__main__':
+    # Set the UPC code for the item we want to look up
+    upc = '073366118238'
+    # Call the api_call function with the UPC code
+    title, brand = api_call(upc)
+    # Print the title and brand of the item
+    print(title)
+    print(brand)
+
--- API/apiKey.py
+++ API/apiKey.py
@@ -1,5 +1,50 @@
+import json
 import requests
-baseUrl = 'http://api.openweathermap.org/data/2.5/forecast'
-parameters = {'appid': 'a03d0d65b881af4e867ca1a058d0e69e','q': 'Roodepoort,ZA'}
-response = requests.get(baseUrl, params=parameters)
+
+
+
+def get_weather_data(city, country):
+    """
+    Get weather data for a given city and country
+
+    Args:
+        city (str): The name of the city
+        country (str): The country code of the city
+
+    Returns:
+        dict: The weather data as a dictionary
+    """
+    # Define the base URL for the OpenWeatherMap API
+    base_url = 'http://api.openweathermap.org/data/2.5/forecast'
+    
+    # Define the parameters for the API request
+    params = {
+        'appid': 'a03d0d65b881af4e867ca1a058d0e69e',
+        'q': f'{city},{country}'
+    }
+    
+    # Send a GET request to the API and retrieve the response
+    response = requests.get(base_url, params=params)
+    
+    # Raise an exception if the request was unsuccessful
+    response.raise_for_status()
+    
+    # Return the weather data as a dictionary
+    return response.json()
+
+
+if __name__ == '__main__':
+    # Set the city and country for which to get the weather data
+    city = 'Roodepoort'
+    country = 'ZA'
+    
+    # Get the weather data for the specified city and country
+    weather_data = get_weather_data(city, country)
+    
+    # Print the weather data as a formatted JSON string
+    print(json.dumps(weather_data, indent=4))
+    
+# Print the contents of the response (should be None)
+print(response.content)
+    print(json.dumps(weather_data, indent=4))
 print(response.content)
